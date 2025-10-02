46a990c2cce874b7c192c4f14be6d00603dcf267
齋藤将平 <shohei@shohei-MacBook.local>
2025-10-02 21:23:41 +0900

    for文で処理を繰り返すようにした

--- main.py
+++ main.py
@@ -11,14 +11,11 @@ document_file_path=r"document.txt"
 senmonyougo_file_path=r"senmonyougo_check.csv"
 setsuzokusi_file_path=r"setsuzokusi_check.csv"
 
-#指摘内容を保管するグローバル変数
-senmonyougo_checklist_log=list()
-setsuzokusi_checklist_log=list()
-
 #listに入っている変数の中身を参照するよ！
+#   [     ファイルパス名,           エラーメッセージ        ]
 filelistdata=[
-    [senmonyougo_file_path, senmonyougo_checklist_log, "●E-3：専門用語が書かれています"],
-    [setsuzokusi_file_path, setsuzokusi_checklist_log, "●E-2：接続詞が誤っています"]
+    [senmonyougo_file_path, "●E-3：専門用語が書かれています"],
+    [setsuzokusi_file_path, "●E-2：接続詞が誤っています"]
 ]
 
 #------------------------------------------------------------------------------------------------
@@ -26,26 +23,19 @@ filelistdata=[
 def function_main(text_data):
     global str_count
     global filelistdata
-    sumcount_a=0                                #文章すべの文字数の合計を格納する変数
+    str_sumcount=0                              #文章すべの文字数の合計を格納する変数
     all_error_count=0                           #全体のエラーのカウントする変数
     function_file_reset(document_file_path)     #document.txtの中身を初期化する関数へ
     sentences=function_list(text_data)          #文章をリスト化する関数へ
+    filelistdata_count=len(filelistdata)
     
     for document_str in sentences:              #1文ごとにfor文を回す。
         str_count=0                             #1文の長さをカウントする変数
-        senmonyougo_count=0                     #専門用語がないかカウントする
-        setsuzokusi_count=0                     #接続詞の誤りをカウントする変数
         str_length_error=0
         
-        global senmonyougo_checklist_log
-        global setsuzokusi_checklist_log
-
-        senmonyougo_checklist_log.clear()
-        setsuzokusi_checklist_log.clear()
-
         #文章をチェックする処理
-        str_count=check_str_count(document_str)             #文字列の長さをカウントする
-        sumcount_a+=str_count                               #取得した1文の長さを加算する
+        str_count=len(document_str)             #文字列の長さをカウントする
+        str_sumcount+=str_count                 #取得した1文の長さを加算する
 
         #document.txtへの書き込み処理
         write_document(document_str,document_file_path)   #1文をdocument.txtに書き込む
@@ -55,12 +45,12 @@ def function_main(text_data):
         str_length_error=write_str_long(str_count,document_file_path)
         all_error_count+=str_length_error
 
-        senmonyougo_count,senmonyougo_checklist_log=check_readcsv(document_str,filelistdata[0][0])     #専門用語がないか確認する
-        setsuzokusi_count,setsuzokusi_checklist_log=check_readcsv(document_str,filelistdata[1][0])     #接続詞に誤りがないかカウントする
-       
-        write_csv_error(senmonyougo_checklist_log,filelistdata[0][2])
-        write_csv_error(setsuzokusi_checklist_log,filelistdata[1][2])
-        all_error_count+=(senmonyougo_count+setsuzokusi_count)
+        for i in range(filelistdata_count):
+            dummylist=list()
+            dummylist.clear()
+            dummy_count,dummylist=check_readcsv(document_str,filelistdata[i][0])
+            write_csv_error(dummylist,filelistdata[i][1])
+            all_error_count+=dummy_count
 
         write_line(document_file_path)                     #1文ごとに区切る線を書く関数へ
     messagebox. showinfo("infomation",f"読み込みが完了しました。\nエラー数{all_error_count} ")
@@ -76,12 +66,6 @@ def function_list(textdata):
     sentences=re.split (r"(?<=[。】＞])",textdata)              #[。】＞]で文章を区切る
     sentences=[s.strip() for s in sentences if s. strip()]     #空白を削除する
     return sentences
-
-#⚫︎1文の文字数をカウントする関数
-def check_str_count(document_a):
-    Str_count=0                 #一文の長さをカウントする変数
-    Str_count=len(document_a)   #document_aの長さを取得する
-    return Str_count
 
 #⚫︎csvファイルでチェックしたものをカウントする関数
 def check_readcsv(document_a,filepath):
@@ -116,8 +100,6 @@ def write_str_count(count,filepath_a):
 
 #⚫︎csvに関する警告文をdocument.txtファイルに書き込む関数
 def write_csv_error(csv_checkeddata,error_str):
-    global document_file_path
-
     if not csv_checkeddata:
         return 
 
